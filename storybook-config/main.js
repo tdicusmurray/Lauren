c8a63410b33bac1b93fda50d731713014bfa0cba
Teddy Murray <tdicusmurray@gmail.com>
2023-11-20 11:30:56 -0800

    initial commit
    UX Interface for Lauren's Mind

--- storybook-config/main.js
+++ storybook-config/main.js
@@ -1,6 +1,37 @@
-module.exports = {
-  core: {
-    configDir: './storybook-config', // Specify your custom configuration directory path
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+          postcssOptions: {
+            plugins: [
+              require('postcss-flexbugs-fixes'), // Fixes known Flexbox bugs
+              require('autoprefixer'), // Adds vendor prefixes for compatibility
+            ],
+          },
+        },
+      },
+    }
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
   },
-  // ... other configuration options
+  docs: {
+    autodocs: "tag",
+  }
 };
+
+export default config;
